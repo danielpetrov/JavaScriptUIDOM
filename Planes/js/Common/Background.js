b23f6fb20970b94d9a690ec1952a6bfbd514d5b4
danielpetrov <danielpetrov666@mail.bg>
2016-01-21 02:34:55 +0200

    v0.1.3 - tricky way to move the stage

--- Planes/js/Common/Background.js
+++ Planes/js/Common/Background.js
@@ -2,21 +2,26 @@ var Stage = function() {
     'use strict';
 
     var stage = document.querySelector('.stage'),
-        windowHeight = window.innerHeight;
+        windowHeight = window.innerHeight,
+        windowWidth = window.innerWidth;
 
     function initStageElements() {
         stage.style.background = "url('img/space.jpeg')";
-        stage.style.width = window.innerWidth + 'px';
+        stage.style.width = windowWidth + 'px';
         stage.style.height = windowHeight + "px";
         stage.style.bottom = 0 + "px";
+        stage.style.right = 0 + "px";
     }
 
     function moveStage() {
-        windowHeight += 0.36;
+        windowHeight += 0.35;
+        windowWidth += 0.31;
         if(windowHeight > 10000){
-            windowHeight = 5000;
+            windowHeight = window.innerHeight;
+            windowWidth = window.innerWidth;
         }
         stage.style.height = windowHeight + "px";
+        stage.style.width = windowWidth + "px";
     }
 
     return {
